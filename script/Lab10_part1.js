82f58f96842672c1caca2a0535716df79329eadd
Tun Win Lat <tunwinlatt@windowslive.com>
2022-04-24 14:45:48 -0700

    Initial Commit

---
+++ script/Lab10_part1.js
@@ -0,0 +1,20 @@
+function changeColor(){
+
+}
+
+var fontColorArray = ["gray","slategray","darkblue","teal","maroon","black"];
+var bgColorArray = ["azure","linen","snow","powderblue","ivory","lightpink","lightyellow"];
+
+var curFontColor=1;
+var curBgColor=1;
+
+function changeFontColor(){
+}
+
+function changeBgColor(){
+}
+
+function changeBg(){
+    
+
+    }
---
+++ script/Lab10_part2.js
@@ -0,0 +1,9 @@
+var topPosition = 200;
+var leftPosition = -200;
+
+function runPuppy() {
+                    
+    var puppy = document.getElementById("puppy");
+    
+// add if it reaches the screen end (you can check with screen.availwidth)					
+}
